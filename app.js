f7c38b98661a0797a12d59f0eed82e64851813e9
Valerii Vitenko <valero2313@gmail.com>
2022-01-04 00:24:26 +0200

    added graphQL schema and graphQl-express endpoints

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 require('dotenv').config();
 
 const { PORT } = process.env;
@@ -7,6 +9,41 @@ const { PORT } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+      `),
+    rootValue: {
+      events: () => {
+        return [
+          'Rough night at hospital',
+          'Balloon shooting',
+          'Great horse riding',
+        ];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
 
 app.get('/');
