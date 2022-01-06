26600526d789f6f57d971944780179c42e84edbc
Valerii Vitenko <valero2313@gmail.com>
2022-01-06 17:05:03 +0200

    added CORS policy

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const graphQlResolvers = require('./graphql/resolvers');
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(authCheck);
 
 app.use(
